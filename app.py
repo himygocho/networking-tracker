1c3b0127b4b47777b27646c94c82c24687259d58
James Simerson <jrsimerson@gmail.com>
2026-07-10 18:36:44 +0900

    Now saves new contacts to CSV

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import csv
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_FILE = os.path.join(BASE_DIR, "contacts.csv")
 
 @app.route("/")
 def home():
@@ -9,15 +13,23 @@ def home():
 @app.route("/add", methods=["POST"])
 def add_contact():
 
+    print(os.getcwd())
+    print(os.path.abspath("contacts.csv"))
+
     name = request.form["name"]
     email = request.form["email"]
     phone = request.form["phone"]
     notes = request.form["notes"]
 
-    print(name)
-    print(email)
-    print(phone)
-    print(notes)
+    with open(CSV_FILE, "a", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow([
+            name,
+            email,
+            phone,
+            notes
+        ])
 
     return "Contact Added"
 
